335b74f0a6ed22929fdc5619f19d449a6da80cb8
ineveraskedforthis <ineveraskedforthis@users.noreply.github.com>
2024-10-24 00:35:31 +0300

    add reply button, fix bug

--- js/main.ts
+++ js/main.ts
@@ -1,1 +1,13 @@
 // window.onload = () => window.scrollTo(0, document.body.scrollHeight);
+
+let reply_buttons = document.querySelectorAll(".reply-button")
+
+reply_buttons.forEach((item) => {
+    let id = parseInt(item.id.split("-")[2]);
+    (item as HTMLElement).onclick = () =>  {
+        let field = document.getElementById('message-area') as HTMLTextAreaElement
+        if (field == null) alert("no message field")
+        else
+            field.value = field.value + ">>" + id
+    }
+})
